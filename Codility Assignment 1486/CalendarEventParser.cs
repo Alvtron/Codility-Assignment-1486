7679a1e80c1a62723c84980ea982dc33da0f1c90
Thomas Angeland <thomas.angeland@gmail.com>
2020-03-31 16:14:35 +0200

    Made should-be-static methods static and shuffled code.

--- Codility Assignment 1486/CalendarEventParser.cs
+++ Codility Assignment 1486/CalendarEventParser.cs
@@ -26,37 +26,6 @@ namespace Codility_Assignment_1486
         }
 
         /// <summary>
-        /// Converts the string to day of week.
-        /// </summary>
-        /// <param name="input">The input string.</param>
-        /// <returns>The day of the week</returns>
-        /// <exception cref="ArgumentException">Day string abbreviation is not valid.</exception>
-        private static DayOfWeek ConvertStringToDayOfWeek(string input)
-        {
-            foreach (DayOfWeek day in Enum.GetValues(typeof(DayOfWeek)))
-            {
-                if (day.ToString().StartsWith(input, StringComparison.InvariantCultureIgnoreCase))
-                {
-                    return day;
-                }
-            }
-            throw new ArgumentException("Day string abbreviation is not valid.");
-        }
-
-        /// <summary>
-        /// Creates the time span.
-        /// </summary>
-        /// <param name="dayOfWeek">The day of week.</param>
-        /// <param name="hours">The hours.</param>
-        /// <param name="minutes">The minutes.</param>
-        /// <returns></returns>
-        private TimeSpan CreateTimeSpan(DayOfWeek dayOfWeek, int hours, int minutes, int seconds)
-        {
-            var startTimeSpan = new TimeSpan(hours, minutes, seconds);
-            return startTimeSpan += TimeSpan.FromDays((int)dayOfWeek);
-        }
-
-        /// <summary>
         /// Parses a string to calendar event meeting according to the syntax defined in this class.
         /// </summary>
         /// <param name="input">The input string.</param>
@@ -87,7 +56,45 @@ namespace Codility_Assignment_1486
             return (T)Activator.CreateInstance(typeof(T), startTimeSpan, endTimeSpan);
         }
 
-        static F GetMatchedValue<F>(GroupCollection match, string key) where F : IConvertible
+        /// <summary>
+        /// Converts the string to day of week.
+        /// </summary>
+        /// <param name="input">The input string.</param>
+        /// <returns>The day of the week</returns>
+        /// <exception cref="ArgumentException">Day string abbreviation is not valid.</exception>
+        private static DayOfWeek ConvertStringToDayOfWeek(string input)
+        {
+            foreach (DayOfWeek day in Enum.GetValues(typeof(DayOfWeek)))
+            {
+                if (day.ToString().StartsWith(input, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    return day;
+                }
+            }
+            throw new ArgumentException("Day string abbreviation is not valid.");
+        }
+
+        /// <summary>
+        /// Creates the time span.
+        /// </summary>
+        /// <param name="dayOfWeek">The day of week.</param>
+        /// <param name="hours">The hours.</param>
+        /// <param name="minutes">The minutes.</param>
+        /// <returns></returns>
+        private static TimeSpan CreateTimeSpan(DayOfWeek dayOfWeek, int hours, int minutes, int seconds)
+        {
+            var startTimeSpan = new TimeSpan(hours, minutes, seconds);
+            return startTimeSpan += TimeSpan.FromDays((int)dayOfWeek);
+        }
+
+        /// <summary>
+        /// Gets the matched value.
+        /// </summary>
+        /// <typeparam name="F">The excpected type of the extracted value.</typeparam>
+        /// <param name="match">The match.</param>
+        /// <param name="key">The key.</param>
+        /// <returns></returns>
+        private static F GetMatchedValue<F>(GroupCollection match, string key) where F : IConvertible
         {
             if (!match.TryGetValue(key, out var group))
             {
